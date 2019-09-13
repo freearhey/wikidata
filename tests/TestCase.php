f022fb453d0909425764bddc3cc573ede1347423
freearhey <free.arhey@gmail.com>
2019-09-13 20:05:18 +0300

    Replaced dummy data with more realistic one

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,18 +7,35 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
   protected $dummy = [
-    'item' => 'http://www.wikidata.org/entity/Q11019',
-    'itemLabel' => 'máquina',
-    'itemDescription' => 'conjunto de elementos móviles y fijos orientados para realizar un trabajo determinado',
-    'itemAltLabel' => 'maquina',
-    'statement' => 'wds:Q11019-18628e57-437b-8a98-6176-13a32b3e02dd',
-    'prop' => 'http://www.wikidata.org/entity/P101',
-    'propertyLabel' => 'campo de trabajo',
-    'propertyValue' => 'http://www.wikidata.org/entity/Q517596',
-    'propertyValueLabel' => 'mechanism',
-    'qualifier' => 'http://www.wikidata.org/entity/P805',
-    'qualifierLabel' => 'statement is subject of',
-    'qualifierValue' => 'http://www.wikidata.org/entity/Q23857720',
-    'qualifierValueLabel' => 'Q23857720',
+    [
+        "item" => "http://www.wikidata.org/entity/Q49546",
+        "itemLabel" => "acetone",
+        "itemDescription" => "chemical compound",
+        "itemAltLabel" => "2-propanone, beta-ketopropane, dimethyl ketone, dimethylketone, ketone propane, methyl ketone, propanone",
+        "prop" => "http://www.wikidata.org/prop/P4952",
+        "propertyLabel" => "safety classification and labelling",
+        "statement" => "http://www.wikidata.org/entity/statement/Q49546-68675f20-4644-b8be-6280-f8cec5399f36",
+        "propertyValue" => "http://www.wikidata.org/entity/Q2005334",
+        "propertyValueLabel" => "Regulation (EC) No. 1272/2008",
+        "qualifier" => "http://www.wikidata.org/entity/P5041",
+        "qualifierLabel" => "GHS hazard statement",
+        "qualifierValue" => "http://www.wikidata.org/entity/Q51844420",
+        "qualifierValueLabel" => "H336"
+    ],
+    [
+        "item" => "http://www.wikidata.org/entity/Q49546",
+        "itemLabel" => "acetone",
+        "itemDescription" => "chemical compound",
+        "itemAltLabel" => "2-propanone, beta-ketopropane, dimethyl ketone, dimethylketone, ketone propane, methyl ketone, propanone",
+        "prop" => "http://www.wikidata.org/prop/P4952",
+        "propertyLabel" => "safety classification and labelling",
+        "statement" => "http://www.wikidata.org/entity/statement/Q49546-68675f20-4644-b8be-6280-f8cec5399f36",
+        "propertyValue" => "http://www.wikidata.org/entity/Q2005334",
+        "propertyValueLabel" => "Regulation (EC) No. 1272/2008",
+        "qualifier" => "http://www.wikidata.org/entity/P5041",
+        "qualifierLabel" => "GHS hazard statement",
+        "qualifierValue" => "http://www.wikidata.org/entity/Q54876503",
+        "qualifierValueLabel" => "EUH066"
+    ]
   ];
 }
