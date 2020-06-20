cea21ed5e2366f8045ffe532e219df0914967069
freearhey <free.arhey@gmail.com>
2020-06-20 15:01:57 +0300

    Added testGetEntityWithoutAliases()

--- tests/Unit/EntityTest.php
+++ tests/Unit/EntityTest.php
@@ -46,6 +46,32 @@ class EntityTest extends TestCase
     $this->assertEquals($aliases, $this->entity->aliases);
   }
 
+  public function testGetEntityWithoutAliases()
+  {
+    $dummy = [
+      [
+          "item" => "http://www.wikidata.org/entity/Q49546",
+          "itemLabel" => "acetone",
+          "wikipediaArticle" => "https://en.wikipedia.org/wiki/Acetone",
+          "itemDescription" => "chemical compound",
+          "itemAltLabel" => NULL,
+          "prop" => "http://www.wikidata.org/prop/P4952",
+          "propertyLabel" => "safety classification and labelling",
+          "statement" => "http://www.wikidata.org/entity/statement/Q49546-68675f20-4644-b8be-6280-f8cec5399f36",
+          "propertyValue" => "http://www.wikidata.org/entity/Q2005334",
+          "propertyValueLabel" => "Regulation (EC) No. 1272/2008",
+          "qualifier" => "http://www.wikidata.org/entity/P5041",
+          "qualifierLabel" => "GHS hazard statement",
+          "qualifierValue" => "http://www.wikidata.org/entity/Q51844420",
+          "qualifierValueLabel" => "H336"
+      ]
+    ];
+
+    $entity = new Entity($dummy, 'en');
+
+    $this->assertEquals([], $entity->aliases);
+  }
+
   public function testGetEntityDescription()
   {
     $this->assertEquals($this->dummy[0]['itemDescription'], $this->entity->description);
